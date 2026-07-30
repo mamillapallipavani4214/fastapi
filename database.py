f006917c0ffd4517f03f50db3835959d0e3a7dce
mamillapallipavani4214 <mpavani596@gmail.com>
2026-07-30 16:18:54 +0530

    Modify database connection settings
    Updated DATABASE_URL and added SSL connection arguments.

--- database.py
+++ database.py
@@ -3,9 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/mobiles_db"
-DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_wjN3XLOMbXUa5Dqz1h0@mysql-15560c42-mpavani596-a3aa.j.aivencloud.com:24476/defaultdb?ssl-mode=REQUIRED"
+DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_wjN3XLOMbXUa5Dqz1h0@mysql-15560c42-mpavani596-a3aa.j.aivencloud.com:24476/defaultdb"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, connect_args={"ssl":{}} )
 
 SessionLocal = sessionmaker(
     autocommit=False,
